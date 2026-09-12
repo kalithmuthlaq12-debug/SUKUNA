a99167404a342235a542a9c58c14220180d59764
A.Z.M Kalith Muthlaq <kalithmuthlaq12@gmail.com>
2026-09-12 10:38:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 //-------------------[ BOT SETTINGS ]------------------// 
 
-// @project_name : KANGO-XMD 
-// @author : Hector 
-// @telegram : http://t.me/official_kango
-// @github : OfficialKango
-// @whatsapp : +233509977126
+// @project_name : SUKUNA-MD 
+// @author : SUKUNA 
+// @telegram : http://t.me/itz_me_R_Sukuna
+// @github : Kalithmuthlaq12
+// @whatsapp : +94782385633
 
 //----------------------[ KANGO-XMD ]----------------------//
 
@@ -20,21 +20,21 @@ global.SESSION_ID = process.env.SESSION_ID || ''
 
 //--------------------[ BOT NAME ]----------------------//
 
-global.botname = process.env.BOT_NAME || 'KANGO-XMD' 
+global.botname = process.env.BOT_NAME || 'SUKUNA-MD' 
 
 //-----------------[ OWNER NUMBER ]------------------//
 
-global.ownernumber = process.env.OWNER_NUMBER || '233509977126' 
+global.ownernumber = process.env.OWNER_NUMBER || '94782385633' 
 
 //--------------------[ SUDO ]--------------------------//
 
-global.sudo = process.env.SUDO ? process.env.SUDO.split(',') : ['233509977126', '233577860202'];
+global.sudo = process.env.SUDO ? process.env.SUDO.split(',') : ['94723381000'];
 // Type additional allowed users here
 //NB: They'll be able to use every functions of the bot without restrictions.
 
 //-----------------[ OWNER NAME ]------------------//
 
-global.ownername = process.env.OWNER_NAME || 'Hector Manuel' 
+global.ownername = process.env.OWNER_NAME || 'SUKUNA' 
 
 //------------[ STICKER PACKNAME ]-----------------//
 
@@ -43,13 +43,13 @@ global.packname = process.env.STICKER_PACK_NAME || "KANGO-XMD"
 //--------------[ COUNTRY TIMEZONE ]------------//
 
 
-global.timezones = 'Africa/Accra';  // Set this to you timezone
+global.timezones = 'Asia/Colombo';  // Set this to you timezone
 
 
 
 //--------------[ STICKER AUTHOR NAME ]------------//
 
-global.author = process.env.STICKER_AUTHOR_NAME || "Hector" 
+global.author = process.env.STICKER_AUTHOR_NAME || "SUKUNA" 
 
 //----------------[ GITHUB DATABASE ]-----------------//
 
@@ -58,17 +58,17 @@ global.dbToken = process.env.GITHUB_TOKEN || "";
 
 //-----------------[ CONTEXT LINK ]--------------------//
 
-global.plink = process.env.PLINK || "https://youtube.com/@official_manuel"
+global.plink = process.env.PLINK || ""
 
 //------------------[ WATERMARK ]--------------------//
 
-global.wm = process.env.GL_WM || "> ©KANGO-XMD"
+global.wm = process.env.GL_WM || "> ©SUKUNA-MD"
 
 //---------------------[ REPLIES ]-----------------------//
 
 global.mess = { 
   done: '*Done*', 
-  success: '©kango-xmd', 
+  success: '©sukuna-md', 
   owner: `*You don't have permission to use this command!*`, 
   group: '*This feature becomes available when you use it in a group!*', 
   admin: '*You’ll unlock this feature with me as an admin!*', 
